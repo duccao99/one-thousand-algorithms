255429c370c9aae62f8f5d295c6c6a2608dc18be
duccao99 <caovanducs@gmail.com>
2021-07-24 14:47:55 +0700

    95

--- Chap3/95.E95.ts
+++ Chap3/95.E95.ts
@@ -1,9 +1,31 @@
+import readline from 'readline';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const recursiveAsyncReadline = function (time: number) {
+  rl.question('Enter number: ', function (num: string) {
+    if (time === 1) {
+      console.log(
+        `Absolute value of number ${num} is ${Math.abs(Number(num))}\n`
+      );
+
+      return rl.close();
+    }
+
+    console.log(
+      `Absolute value of number ${num} is ${Math.abs(Number(num))}\n`
+    );
+    recursiveAsyncReadline(time - 1);
+  });
+};
+
 function E95() {
-  /**
-   * Write a program to input 3 float numbers, replace all negative numbers with
-   * its absolute
-   *
-   */
+  console.log('Enter three numbers');
+
+  recursiveAsyncReadline(3);
 }
 
 E95();
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # thay-khang-one-thousand-algorithm-exercises-typescriptlang
-1000 bài tập của thầy Nguyễn Tấn Trần Minh Khang
+
+1000 bài tập - thầy Nguyễn Tấn Trần Minh Khang
