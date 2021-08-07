f3cb1935376d24a42f70543eb2701e9b89ab46c4
duccao99 <caovanducs@gmail.com>
2021-08-07 13:07:58 +0700

    106

--- Chap3/106.E106.ts
+++ Chap3/106.E106.ts
@@ -1,45 +1,13 @@
 /**
- * Write a program enter an integer have two digit
- * print out the reading of this integer
+ * Write a program to enter a number which have three digits
+ * print the way that how to call this number
  *
- * + is two digit ?
- *   + 10 - ten
- *   + 11 - eleven
- *   + 12 - twelve
- *   + 13 - thirteen
- *   + 14 - fourteen
- *   + 15 - fifteen
- *   + 16 - sixteen
- *   + 17 - seventeen
- *   + 18 - eighteen
- *   + 19 - nineteen
- *   + 20 - twenty
- *   + 21 - twenty one
- *   ...
- *   + 29 - twenty nine
- *   + 30 - thirty
- *   + ..
- *   + 40 - forty
- *   + 41 - forty one
- *   + ...
- *   + 50 - fifty
- *   +..
- *   + 60 - sixty
- *   +..
- *   + 70 - seventy
- *   ..
- *   + 80 - eighty
- *   ..
- *   + 90 - ninety
- *   ..
- *   + 99 - ninety nine
+ * Idea:
+ * + reuse 105
+ * + check if n was three digits number
+ *  - say
  *
- *
- * + error
- *
- * + Try to make third+teen, four+teen
- *
- *
+ * + else print error
  */
 
 import rl from 'readline';
@@ -118,99 +86,92 @@ function handleNinety(number: number): string {
 }
 
 function handleTwenty(number: number): string {
-  // Too much code - optimize it!
   var prefix: string = 'Twenty ';
-  var postfix: string = '';
-  if (number % 10 === 1) {
-    postfix = 'One';
-  } else if (number % 10 === 2) {
-    postfix = 'Two';
-  } else if (number % 10 === 3) {
-    postfix = 'Third';
-  } else if (number % 10 === 4) {
-    postfix = 'Four';
-  } else if (number % 10 === 5) {
-    postfix = 'Five';
-  } else if (number % 10 === 6) {
-    postfix = 'Six';
-  } else if (number % 10 === 7) {
-    postfix = 'Seven';
-  } else if (number % 10 === 8) {
-    postfix = 'Eight';
-  } else if (number % 10 === 9) {
-    postfix = 'Nine';
-  }
-
+  var postfix: string = handlePostfixOneToTen(number);
   return prefix + postfix;
 }
 
-async function E105() {
-  const ask = 'Enter number: ';
-  const answer = await readAsyncStdin(ask);
-  const number = Number(answer);
+function handleZeroToTen(number: number): string {
   var ret: string = '';
+  switch (number) {
+    case 0:
+      ret = '';
+      break;
+    case 1:
+      ret = 'One';
+      break;
+    case 2:
+      ret = 'Two';
+      break;
+    case 3:
+      ret = 'Three';
+      break;
+    case 4:
+      ret = 'Fourth';
+      break;
+    case 5:
+      ret = 'Five';
+      break;
+    case 6:
+      ret = 'Six';
+      break;
+    case 7:
+      ret = 'Seven';
+      break;
+    case 8:
+      ret = 'Eight';
+      break;
+    case 9:
+      ret = 'Nine';
+      break;
+    case 10:
+      ret = 'Ten';
+      break;
 
-  if (isNaN(number)) {
-    ret = 'Input is not a number';
-  } else {
-    var count = 0;
-
-    for (let i = number; i !== 0; i = Math.floor(i / 10)) {
-      count++;
-    }
-    if (count !== 2) {
-      ret = 'Number must have two digit';
-    } else {
-      if (number === 10) {
-        ret = 'Ten';
-      } else if (number === 11) {
-        ret = 'Eleven';
-      } else if (number === 12) {
-        ret = 'Twelve';
-      } else if (number >= 13 && number <= 19) {
-        var teen: string = 'teen';
-        var prefix: string = '';
-        if (number % 10 === 3) {
-          prefix = 'Third';
-        } else if (number % 10 === 4) {
-          prefix = 'Four';
-        } else if (number % 10 === 5) {
-          prefix = 'Five';
-        } else if (number % 10 === 6) {
-          prefix = 'Six';
-        } else if (number % 10 === 7) {
-          prefix = 'Seven';
-        } else if (number % 10 === 8) {
-          prefix = 'Eigh';
-        } else if (number % 10 === 9) {
-          prefix = 'Nine';
-        }
-        ret = prefix + teen;
-      } else if (number >= 20 && number <= 29) {
-        ret = handleTwenty(number);
-      } else if (number >= 30 && number <= 39) {
-        ret = handleThirty(number);
-      } else if (number >= 40 && number <= 49) {
-        ret = handleForty(number);
-      } else if (number >= 50 && number <= 59) {
-        ret = handleFifty(number);
-      } else if (number >= 60 && number <= 69) {
-        ret = handleSixty(number);
-      } else if (number >= 70 && number <= 79) {
-        ret = handleSeventy(number);
-      } else if (number >= 80 && number <= 89) {
-        ret = handleEighty(number);
-      } else if (number >= 90 && number <= 99) {
-        ret = handleNinety(number);
-      }
-    }
+    default:
+      break;
   }
-
-  crl.close();
-
   return ret;
 }
-function E105SecondWay(number: number): string {
+
+function handleElevenToNineTeen(number: number): string {
+  var ret: string = '';
+  switch (number) {
+    case 11:
+      ret = 'Eleven';
+      break;
+    case 12:
+      ret = 'Twelve';
+      break;
+    case 13:
+      ret = 'Thirteen';
+      break;
+    case 14:
+      ret = 'Fourteen';
+      break;
+    case 15:
+      ret = 'Fifteen';
+      break;
+    case 16:
+      ret = 'Sixteen';
+      break;
+    case 17:
+      ret = 'Seventeen';
+      break;
+    case 18:
+      ret = 'Eighteen';
+      break;
+    case 19:
+      ret = 'Nineteen';
+      break;
+
+    default:
+      break;
+  }
+  return ret;
+}
+
+function handleSayTwoDigitsNumber(number: number): string {
   var ret = '';
   if (isNaN(number)) {
     ret = 'Input is not a number';
@@ -271,13 +232,137 @@ function E105SecondWay(number: number): string {
   return ret;
 }
 
-async function main() {
-  const ret = await E105();
-  console.log(chalk.yellowBright(ret));
+function countDigitNumber(number: number): number {
+  if (number === 0) return 1;
+  var count = 0;
+  for (let i = number; i !== 0; i = Math.floor(i / 10)) count++;
+  return count;
+}
 
-  for (let i = 50; i < 80; ++i) {
-    console.log(E105SecondWay(i));
+function handleSayThreeDigitsNumber(number: number) {
+  var first_digit = Math.floor(number / 100);
+  var last_two_digits = number % 100;
+  var prefix: string = '';
+
+  var number_digits = countDigitNumber(Number(last_two_digits));
+  var ret: string = '';
+  console.log(number_digits);
+
+  if (number_digits === 2) {
+    var postfix: string = handleSayTwoDigitsNumber(last_two_digits);
+    switch (first_digit) {
+      case 1:
+        prefix = 'One hundred  ';
+        break;
+      case 2:
+        prefix = 'Two hundred  ';
+
+        break;
+      case 3:
+        prefix = 'Three hundred  ';
+
+        break;
+      case 4:
+        prefix = 'Four hundred ';
+
+        break;
+      case 5:
+        prefix = 'Five hundred ';
+
+        break;
+      case 6:
+        prefix = 'Six hundred ';
+
+        break;
+      case 7:
+        prefix = 'Seven hundred ';
+
+        break;
+      case 8:
+        prefix = 'Eight hundred ';
+
+        break;
+      case 9:
+        prefix = 'Nine hundred ';
+
+        break;
+
+      default:
+        break;
+    }
+    ret = prefix + postfix;
+  } else if (number_digits === 1) {
+    var postfix: string = handleZeroToTen(last_two_digits);
+    switch (first_digit) {
+      case 1:
+        prefix = 'One hundred ';
+        break;
+      case 2:
+        prefix = 'Two hundred ';
+
+        break;
+      case 3:
+        prefix = 'Three hundred ';
+
+        break;
+      case 4:
+        prefix = 'Four hundred ';
+
+        break;
+      case 5:
+        prefix = 'Five hundred ';
+
+        break;
+      case 6:
+        prefix = 'Six hundred ';
+
+        break;
+      case 7:
+        prefix = 'Seven hundred ';
+
+        break;
+      case 8:
+        prefix = 'Eight hundred ';
+
+        break;
+      case 9:
+        prefix = 'Nine hundred ';
+
+        break;
+
+      default:
+        break;
+    }
+    ret = prefix + postfix;
   }
+  return ret;
+}
+
+async function E106() {
+  const ask = 'Enter number: ';
+  const answer = await readAsyncStdin(ask);
+  const number = Number(answer);
+  var ret: string = '';
+
+  if (isNaN(number)) {
+    ret = 'Input is not a number';
+  } else {
+    var total_digits_number = countDigitNumber(number);
+    if (total_digits_number !== 3) {
+      ret = 'Number must have three digit';
+    } else {
+      ret = handleSayThreeDigitsNumber(number);
+    }
+  }
+
+  crl.close();
+
+  return ret;
+}
+
+async function main() {
+  const ret = await E106();
+  console.log(chalk.yellowBright(ret));
 }
 
 main();
---
+++ test.ts
@@ -0,0 +1,9 @@
+var a = 123;
+var b = 231;
+var c = 101;
+var d = 100;
+
+console.log(a % 100);
+console.log(b % 100);
+console.log(c % 100);
+console.log(d % 100);
