a6500df7fff0011d2d9c49f974fa704b1d1b8714
tcvduc <caovanducs@gmail.com>
2022-02-22 19:41:04 +0700

    393

---
+++ Chap6-Matrix/6.MatrixHandleTech/393.js
@@ -0,0 +1,182 @@
+/**
+ * Problem: Permutate two rows in matrix
+ * 
+ * 
+ * Understanding the problem
+ * 
+ * - Matrix
+ * -- 0 1 2
+ * 0| 1 6 3
+ * 1| 6 28 7
+ * 2| 28 16 8
+ * 3| 7 11 13
+ * 
+ * - column_1 = 1
+ * - column_2 = 2
+ * 
+ * - ret
+ * -- 0 1 2
+ * 0| 1 3 6
+ * 1| 28 7 28
+ * 2| 6 8 16
+ * 3| 7 13 11
+ * 
+ * - m[3][1] = m[3][2]
+ * - m[2][1] = m[2][2]
+ * - m[1][1] = m[1][2]
+ * - m[0][1] = m[0][2]
+ * 
+ * -> i from m.length - 1 to 0
+ *    const temporary = m[i][column_1] 
+ *    m[i][column_1] = m[i][column_2]
+ *    m[i][column_2] = temporary
+ * 
+ * 
+ * 
+ *  
+ * 
+ *
+ *
+ * /
+  
+/**
+ *
+ * @param {Array<Array>} m
+ */
+function fx(m, column_1, column_2) {
+  for (let i = m.length - 1; i >= 0; --i) {
+    const temporary = m[i][column_1];
+    m[i][column_1] = m[i][column_2];
+    m[i][column_2] = temporary;
+  }
+  return m;
+}
+
+/**
+ *
+ * @param {Array<Array>} m
+ */
+function advanceLogMatrix(m) {
+  console.log("-----Matrix-----");
+  /**
+   *
+   * @param {Number} n
+   */
+  function getNumberDigits(n) {
+    let ret = 0;
+
+    while (n !== 0) {
+      n = Math.floor(n / 10);
+      ret++;
+    }
+
+    return ret;
+  }
+
+  let columnIndex = "--";
+  for (let i = 0; i <= m[0].length - 1; ++i) {
+    let space = "";
+    let numberDigits = getNumberDigits(m[0][i]);
+    for (let j = numberDigits - 1; j >= 0; --j) {
+      space += " ";
+    }
+    columnIndex += i + space;
+  }
+  console.log(columnIndex);
+  for (let i = 0; i <= m.length - 1; ++i) {
+    let row = i + "|";
+    for (let j = 0; j <= m[i].length - 1; ++j) {
+      row += m[i][j] + " ";
+    }
+    console.log(row);
+  }
+  console.log("---------------");
+}
+
+/**
+ *
+ * @param {Array} a
+ * @param {any} e
+ */
+function push(a, e) {
+  let ret = new Array(a.length + 1);
+  ret[ret.length - 1] = e;
+  for (let i = ret.length - 2; i >= 0; --i) {
+    ret[i] = a[i];
+  }
+  return ret;
+}
+
+function test1() {
+  /**
+   *
+   * @param {Number} rows
+   * @param {Number} columns
+   *
+   */
+  function generateMatrix(rows, columns) {
+    let ret = [];
+    for (let i = rows - 1; i >= 0; --i) {
+      let row = [];
+      for (let j = columns - 1; j >= 0; --j) {
+        row = push(row, generateNumber(0, 100));
+      }
+      ret = push(ret, row);
+    }
+    return ret;
+  }
+
+  /**
+   *
+   * @param {Number} from
+   * @param {Number} to
+   *
+   */
+  function generateNumber(from, to) {
+    let random = Math.random();
+    let ret = random * to;
+
+    if (ret < from || ret > to) {
+      return generateNumber(from, to);
+    }
+
+    return Math.floor(ret);
+  }
+
+  const rows_1 = 1;
+  const rows_2 = 2;
+  const rows_3 = 3;
+  const rows_4 = 4;
+
+  const columns_1 = 1;
+  const columns_2 = 2;
+  const columns_3 = 3;
+  const columns_4 = 4;
+
+  const m2 = generateMatrix(rows_2, columns_3);
+  const m4 = generateMatrix(rows_3, columns_4);
+  const m5 = generateMatrix(rows_4, columns_3);
+
+  const column_index_0 = 0;
+  const column_index_1 = 1;
+  const column_index_2 = 2;
+  const column_index_3 = 3;
+
+  const row_index_0 = 0;
+  const row_index_1 = 1;
+  const row_index_2 = 2;
+  const row_index_3 = 3;
+
+  advanceLogMatrix(m2);
+  advanceLogMatrix(fx(m2, column_index_0, column_index_1));
+
+  advanceLogMatrix(m4);
+  advanceLogMatrix(fx(m4, column_index_0, column_index_2));
+
+  advanceLogMatrix(m5);
+  advanceLogMatrix(fx(m5, column_index_2, column_index_1));
+}
+
+{
+  test1();
+}
