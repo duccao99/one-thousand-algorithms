e93e4fac657e7156975f4e8eee7e56075ba97e26
tcvduc <caovanducs@gmail.com>
2022-02-27 13:05:56 +0700

    implement advance log array

--- b.js
+++ b.js
@@ -1,44 +1,19 @@
-function getDivisors(n) {
-  let divisors = [];
-  for (let i = 1; i <= n; ++i) {
-    if (n % i === 0) {
-      divisors.push(i);
-    }
-  }
-  return divisors.join(" ");
-}
-
-function isPrime(n) {
-  for (let i = 2; i < n; ++i) {
-    if (n % i === 0) {
-      return false;
-    }
-  }
-  return true;
-}
-
 /**
+ * -------0 1 2 3 4
+ * - a = [1,2,3,4,5]
+ * ---------0 1 2 3 4
+ * - ret = [2,3,4,5,1]
  *
- * @param {Array} a
+ *
+ *
+ *
  */
-function getMaxPrime(a) {
-  let maxPrime = Number.NEGATIVE_INFINITY;
-  for (let i = a.length - 1; i >= 0; --i) {
-    if (maxPrime < a[i] && isPrime(a[i])) {
-      maxPrime = a[i];
-    }
-  }
-  return maxPrime;
+const a = [1, 2, 3, 4, 5];
+const temporary = a[0];
+for (let i = 0; i < a.length - 1; ++i) {
+  const temporary = a[i];
+  a[i] = a[i + 1];
+  a[i + 1] = temporary;
 }
-
-{
-  let primes = [];
-  for (let i = 1900; i <= 1998; ++i) {
-    primes.push(i);
-  }
-  console.log(
-    `Greatest prime number if range between 1900..1998 is: ${getMaxPrime(
-      primes
-    )}`
-  );
-}
+a[a.length - 1] = temporary;
+console.log(a);
