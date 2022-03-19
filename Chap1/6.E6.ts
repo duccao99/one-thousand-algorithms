af54149c3e42564d976ca664ded6876c03750a11
tcvduc <caovanducs@gmail.com>
2022-03-19 18:47:28 +0700

    redo 6

--- Chap1/6.E6.ts
+++
@@ -1,9 +0,0 @@
-function E6(n: number) {
-  // 1/1*2+1/2*3+...+1/n*(n+1)
-
-  var ret = 1;
-  for (let i = 2; i <= n; ++i) ret += 1 / (i * (i + 1));
-  return ret;
-}
-const n = 4;
-console.log(E6(n));
---
+++ Chap1/6.js
@@ -0,0 +1,34 @@
+/**
+ * Problem: write a function to calculate the
+ * S(n) = 1/(1 x 2) + 1/(2 x 3) + .. +  1/(n x (n + 1))
+ *
+ * Understanding the problem
+ * - What is  S(n) = 1/(1 x 2) + 1/(2 x 3) + .. +  1/(n x (n + 1))
+ * - Example:
+ *   + S(1) = 1/(1 x 2)
+ *   + S(2) = 1/(1 x 2) + 1/(2 x 3)
+ *   + S(3) = 1/(1 x 2) + 1/(2 x 3) + 1/(3 x 4)
+ *
+ *
+ * Approach : use loop
+ * + ret = 0
+ * + i from n to 1
+ *   + ret = ret + 1/(i x (i+1))
+ *
+ */
+
+function fx(n) {
+  let ret = 0;
+
+  for (let i = n; i >= 1; --i) {
+    ret = ret + 1 / (i * (i + 1));
+  }
+
+  return ret;
+}
+
+{
+  for (let n = 100; n >= 1; --n) {
+    console.log(`n = ${n} - S(${n}) = ${fx(n)}`);
+  }
+}
