8044a4d8c8d9993d8f67dafcf9b57dda279ade66
tcvduc <caovanducs@gmail.com>
2022-05-17 01:17:17 +0700

    does some array problems

---
+++ Chap19-Array/956.js
@@ -0,0 +1,45 @@
+/**
+ * - problem: sort array of users in descending order on userId field
+ *
+ * - a = [
+ *  {userId:1, userName: "user01"},
+ *  {userId:3, userName: "user03"},
+ *  {userId:2, userName: "user02"}
+ * ]
+ *
+ * - ret = [
+ *  {userId:3, userName: "user03"}
+ *  {userId:2, userName: "user02"},
+ *  {userId:1, userName: "user01"},
+ * ]
+ *
+ *
+ */
+
+/**
+ *
+ * @param {{userId:number, userName: string}[]} a
+ */
+function f(a) {
+  for (let i = a.length - 1; i >= 0; --i) {
+    for (let j = i - 1; j >= 0; --j) {
+      if (a[j].userId < a[i].userId) {
+        const temporary = a[i];
+        a[i] = a[j];
+        a[j] = temporary;
+      }
+    }
+  }
+  return a;
+}
+
+const a = [
+  { userId: 1, userName: "user01" },
+  { userId: 3, userName: "user03" },
+  { userId: 4, userName: "user04" },
+  { userId: 2, userName: "user02" },
+  { userId: 6, userName: "user06" },
+  { userId: 5, userName: "user05" },
+];
+
+console.log(f(a));
