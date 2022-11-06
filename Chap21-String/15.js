5de8e562b9f3d00ee3e4fa07df4d14e311745b1c
tcvduc <caovanducs@gmail.com>
2022-11-06 13:48:22 +0700

    train string

---
+++ Chap21-String/15.js
@@ -0,0 +1,12 @@
+const a = ["Terrifier 2", "Terrifier 3", "film 3", "film 4"];
+const s = "ier";
+let ret = [];
+
+for (let i = a.length - 1; i >= 0; --i) {
+  const regex = new RegExp(s, "g");
+  if (regex.test(a[i])) {
+    ret.push(a[i]);
+  }
+}
+
+console.log(ret);
